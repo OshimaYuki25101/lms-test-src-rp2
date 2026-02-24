cdb868312afcedc672aab59d73bf9539247f7a75
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-24 14:06:10 +0900

    add:Case10.java 受講生勤怠登録正常系のテストコード作成
    test01、02はCase09のtest01、02と同じため、割愛
    
    test03()メソッド
    WebElementを利用し、ヘッダーの「勤怠」リンクを押下し勤怠管理画面に遷移
    タイトルとURL、勤怠情報が期待値通りかを確認するテストを作成した。
    【期待値は以下の通り】
    タイトル：勤怠情報変更｜LMS
    URL：http://localhost:8000/lms/attendance/detail
    勤怠情報：isDisplayed(True)
    
    test04()メソッド
    WebElementを利用し、「出勤」ボタンを押下。
    タイトルと出勤時間が期待値通りかを確認するテストを作成した。
    【期待値は以下の通り】
    タイトル：勤怠情報変更｜LMS
    出勤時間：テストを実行した時間[isDisplayed(True)]
    
    test05()メソッド
    WebElementを利用し、「退勤」ボタンを押下。
    タイトルと退勤時間が期待値通りかを確認するテストを作成した。
    【期待値は以下の通り】
    タイトル：勤怠情報変更｜LMS
    退勤時間：テストを実行した時間[isDsplayed(True)]

--- test_code_creation_oshimayuki/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_oshimayuki/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +38,81 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		webDriver.get("http://localhost:8000/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		assertEquals("ログイン", webDriver.findElement(By.tagName("h2")).getText());
+		getEvidence(new Object() {});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.id("password")).sendKeys("TestUser001");
+		webDriver.findElement(By.className("btn-primary")).click();
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		assertEquals("http://localhost:8000/lms/course/detail", webDriver.getCurrentUrl());
+		getEvidence(new Object() {});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		webDriver.findElement(By.linkText("勤怠")).click();
+		
+		pageLoadTimeout(30);
+	
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept();
+		
+		pageLoadTimeout(20);
+		
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+		assertEquals("http://localhost:8000/lms/attendance/detail", webDriver.getCurrentUrl());
+		assertTrue(webDriver.findElement(By.tagName("td")).isDisplayed());
+		
+		getEvidence(new Object() {});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+		webDriver.findElement(By.name("punchIn")).click();
+		
+		pageLoadTimeout(30);
+		
+		Alert alert = webDriver.switchTo().alert();
+		alert.dismiss();
+		
+		pageLoadTimeout(20);
+		
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+		assertTrue(webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[3]/div/table/tbody/tr[1]/td[3]")).isDisplayed());
+		
+		getEvidence(new Object() {});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+		webDriver.findElement(By.name("punchOut")).click();
+		
+		pageLoadTimeout(30);
+		
+		Alert alert = webDriver.switchTo().alert();
+		alert.dismiss();
+		
+		pageLoadTimeout(20);
+		
+		assertEquals("勤怠情報変更｜LMS", webDriver.getTitle());
+		assertTrue(webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[3]/div/table/tbody/tr[1]/td[4]")).isDisplayed());
+		
+		getEvidence(new Object() {});
 	}
 
 }
